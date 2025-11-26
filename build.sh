acd7836266f31ff368383b6c938b60cbd6cb826c
JP Meijers <jp.meijers@canonical.com>
2025-11-26 15:59:21 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -51,7 +51,7 @@ printf -v llm_pieces "%s|" "${components[@]}"
 
 echo "Generating new snapcraft.yaml"
 # TODO: generate this list dynamically
-essentials="version|app-scripts|engines|cli|go-chat-client|common-runtime-dependencies|opencl-driver"
+essentials="version|scripts|engines|cli|go-chat-client|common-runtime-dependencies|opencl-driver"
 
 # Copy snap/snapcraft.yaml to snapcraft.yaml, retaining only the parts and components that match the regex
 yq "explode(.) |
