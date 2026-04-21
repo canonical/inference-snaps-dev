45f8f4ea19a77b130ab85068eb8143779c62b941
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-04-21 12:18:17 +0200

    Make wiping mandatory

--- build.sh
+++ build.sh
@@ -1,41 +1,12 @@
 #!/bin/bash -eu
 
-help() {
-  echo "Usage:" >&2
-  echo "  $0 [--wipe] [--help] [snapcraft-pack-options...]" >&2
-  echo "" >&2
-  echo "Options:" >&2
-  echo "  -h, --help  Show this help message" >&2
-  echo "  --wipe    Remove existing .snap and .comp before packing" >&2
-}
-
-wipe=false
-snapcraft_args=()
-
-while (($#)); do
-  case "$1" in
-    -h|--help)
-      help
-      exit 0
-      ;;
-    --wipe)
-      wipe=true
-      ;;
-    --)
-      shift
-      snapcraft_args+=("$@")
-      break
-      ;;
-    *)
-      help
-      exit 1
-      ;;
-  esac
-  shift
-done
-
-if [[ "$wipe" == true ]]; then
-    rm -fv *.snap *.comp
+if [[ ! -f snap/snapcraft.yaml ]]; then
+	echo "Error: snap/snapcraft.yaml not found. Run this script from the project's root." >&2
+	exit 1
 fi
 
-snapcraft -v pack "${snapcraft_args[@]}"
+echo "➤ Removing old snap and component files..."
+rm -fv *.snap *.comp
+
+echo "➤ Packing snap and components..."
+snapcraft -v pack
